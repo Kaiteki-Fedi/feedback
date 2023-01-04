0215667a4012baab8763c99054174ddbd8448168
ajiiisai <ajiiisai@protonmail.com>
2023-01-04 22:41:15 +0100

    Implement initial testing

--- src/app/api/feedback.rs
+++ src/app/api/feedback.rs
@@ -37,3 +37,107 @@ pub async fn create(
 
     HttpResponse::Ok().json(query_result.unwrap())
 }
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{
+        test::{init_service, read_body, read_body_json, TestRequest},
+        web::Data,
+        App,
+    };
+    use serde_json::json;
+
+    use crate::app::{self, db::get_connection_pool, models::feedback::Feedback};
+
+    #[actix_rt::test]
+    async fn create_feedback() {
+        let con_pool = get_connection_pool();
+        let app = init_service(
+            App::new()
+                .app_data(Data::new(con_pool.clone()))
+                .configure(app::init::initialize),
+        )
+        .await;
+
+        let category_test = "General";
+        let message_test = "App is slow";
+
+        let request_body = json!({
+            "category": category_test,
+            "message": message_test,
+            "email": "test@test.com",
+            "device_details": {
+                "version_name": "version1",
+                "platform": "Android",
+                "platform_version": "30",
+                "branch": "main"
+            },
+            "exceptions": []
+        });
+
+        let resp = TestRequest::post()
+            .uri("/feedback")
+            .set_json(&request_body)
+            .send_request(&app)
+            .await;
+
+        assert!(resp.status().is_success(), "Failed create feedback");
+
+        let feedback: Feedback = read_body_json(resp).await;
+
+        assert_eq!(feedback.category, category_test);
+        assert_eq!(feedback.message, message_test);
+    }
+
+    #[actix_rt::test]
+    async fn get_feedbacks() {
+        let con_pool = get_connection_pool();
+        let app = init_service(
+            App::new()
+                .app_data(Data::new(con_pool.clone()))
+                .configure(app::init::initialize),
+        )
+        .await;
+
+        let request_body = json!({
+            "category": "General",
+            "message": "App is slow :(",
+            "email": "test@test.com",
+            "device_details": {
+                "version_name": "version1",
+                "platform": "Android",
+                "platform_version": "30",
+                "branch": "main"
+            },
+            "exceptions": []
+        });
+
+        let resp1 = TestRequest::post()
+            .uri("/feedback")
+            .set_json(&request_body)
+            .send_request(&app)
+            .await;
+
+        assert!(resp1.status().is_success(), "Failed to add feedback 1");
+
+        let resp2 = TestRequest::post()
+            .uri("/feedback")
+            .set_json(&request_body)
+            .send_request(&app)
+            .await;
+
+        assert!(resp2.status().is_success(), "Failed to add feedback 2");
+
+        let resp3 = TestRequest::get().uri("/feedback").send_request(&app).await;
+
+        assert!(resp3.status().is_success(), "Failed to list feedbacks");
+
+        let body = read_body(resp3).await;
+        let feedbacks = serde_json::from_slice::<Vec<Feedback>>(&body).unwrap();
+
+        assert!(
+            feedbacks.len() == 2,
+            "Number of feedbacks does not match up"
+        );
+    }
+}
--- src/app/db/mod.rs
+++ src/app/db/mod.rs
@@ -1,16 +1,38 @@
 use diesel::r2d2::ConnectionManager;
 use diesel::SqliteConnection;
+use diesel_migrations::MigrationHarness;
+use diesel_migrations::{embed_migrations, EmbeddedMigrations};
 use r2d2::Pool;
 
 pub type DbPool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
 
 pub fn get_connection_pool() -> DbPool {
-    let database_url: String = crate::app::config::get("database_url");
+    if cfg!(test) {
+        println!("Connecting to in-memory database...");
+        let manager = ConnectionManager::<SqliteConnection>::new(":memory:");
 
-    println!("Connecting to database {}...", database_url);
-    let manager = ConnectionManager::<SqliteConnection>::new(database_url);
+        let pool = Pool::builder()
+            .build(manager)
+            .expect("Failed to create database connection pool.");
 
-    Pool::builder()
-        .build(manager)
-        .expect("Failed to create database connection pool.")
+        let mut con = pool.get().unwrap();
+
+        let test: &mut SqliteConnection = &mut con;
+
+        println!("Running migrations...");
+
+        test.run_pending_migrations(MIGRATIONS).unwrap();
+
+        pool
+    } else {
+        let database_url: String = crate::app::config::get("database_url");
+
+        println!("Connecting to database {}...", database_url);
+        let manager = ConnectionManager::<SqliteConnection>::new(database_url);
+
+        Pool::builder()
+            .build(manager)
+            .expect("Failed to create database connection pool.")
+    }
 }
